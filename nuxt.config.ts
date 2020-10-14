c6205ceda22a394fbd1e54f90e61e9cc862c0816
daichi.sugiyama <daichi.sugiyama@northdetail.co.jp>
2020-10-14 10:47:13 +0900

    Vuetifyをインストール

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,9 +13,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '@/assets/scss/app.scss'
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
@@ -26,12 +24,12 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
+    '@nuxtjs/vuetify',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    '@nuxt/typescript-build',
-    ['bootstrap-vue/nuxt', { css: false }]
+    '@nuxt/typescript-build'
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
