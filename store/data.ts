bb29accbcebb1b8cc50396b448abd15d0bfe2d35
daichi.sugiyama <daichi.sugiyama@northdetail.co.jp>
2020-10-27 15:08:00 +0900

    listのストアを実装

--- store/data.ts
+++ store/data.ts
@@ -4,23 +4,49 @@ import { listType, cardType } from '~/models/DataType'
 // stateFactory: true → Vuex をモジュールモードで扱うために指定
 @Module({ stateFactory: true, namespaced: true, name: 'data' })
 export default class Data extends VuexModule {
-  cards: cardType[] = [];
-  lists: listType[] = [];
+  data: listType[] = [];
+
+  /**
+   * listを追加
+   * @param title
+   */
+  @Mutation
+  addList(title: string) {
+    const lists: listType[] = this.data;
+    const list: listType = {
+      // リストがない場合、cardId = 0
+      // リストがある場合、cardId = リストの最終要素の id + 1
+      listId: this.data.length === 0 ? 0 : this.data[lists.length - 1].listId + 1,
+      listTitle: title,
+      cardData: []
+    }
+    lists.push(list)
+  }
+
+  /**
+   * listを削除
+   * @param list
+   */
+  @Mutation
+  removeList(list: listType) {
+    this.data.splice(this.data.indexOf(list), 1)
+  }
+
 
   /**
    * cardを追加
    * @param title card カードタイトル
    */
   @Mutation
-  addCard(title: string) {
-    const cards: cardType[] = this.cards;
+  addCard(title: string, list: listType) {
+    const cards: cardType[] = list.cardData;
     const card: cardType = {
       // カードがない場合、cardId = 0
       // カードがある場合、cardId = カードの最終要素の id + 1
       cardId: cards.length === 0 ? 0 : cards[cards.length - 1].cardId + 1,
       cardTitle: title
     }
-    this.cards.push(card)
+    cards.push(card)
   }
 
   /**
@@ -28,8 +54,17 @@ export default class Data extends VuexModule {
    * @param card
    */
   @Mutation
-  removeCard(card: cardType) {
-    this.cards.splice(this.cards.indexOf(card), 1)
+  removeCard(card: cardType, list: listType) {
+    list.cardData.splice(list.cardData.indexOf(card), 1)
+  }
+
+  /**
+   * listを編集
+   * @param list title list リストタイトル
+   */
+  @Mutation
+  updateList(title: string, list: listType) {
+    list.listTitle = title
   }
 
   /**
